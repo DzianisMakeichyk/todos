fce392f8687beb763a18a6715afc938cca18a918
dzianis <dzianis.makeichyk@gmail.com>
2018-06-11 10:19:28 +0200

    add ease header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import TodoHeader from './components/TodoHeader'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <TodoHeader />
        <h1>Hello</h1>
       </div>
     );
